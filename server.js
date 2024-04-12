e1aea5a559f60c2e1c9e2f6fe0cd04db296af6ea
Téo Salmeron <teo.salmeron2601@gmail.com>
2024-04-12 23:15:02 +0200

    fixed path to build

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use("/images", cors(), express.static(path.join(__dirname, "uploads/")));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
