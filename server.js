620724aef6400862f89ecee9d0a68d1354bce332
Téo Salmeron <teo.salmeron2601@gmail.com>
2024-04-12 23:39:24 +0200

    test4

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use("/images", cors(), express.static(path.join(__dirname, "uploads/")));
+app.use(express.static(path.join(__dirname, "../client/build")))
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build"))
+})
 const authRouter = require("./routes/Auth")
 app.use("/auth", authRouter)
 
